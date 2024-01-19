8aa36bb23e83c57b43651911a5495599b6582687
tsehla <petrusnk@gmail.com>
2024-01-19 21:43:17 +0200

    mib

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+// cards 
+console.clear();
+
+var flkty = new Flickity('.card-wrap', {
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+  wrapAround: true,
+  adaptiveHeight: true,
+  setGallerySize: false,
+  selectedAttraction: 0.05,
+  freeScrollFriction: .1 
+});
