d306c535ad3e82f915f29195aaf565e5768f76df
tsehla <petrusnk@gmail.com>
2024-01-20 22:48:29 +0200

    mib

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ var flkty = new Flickity('.card-wrap', {
   selectedAttraction: 0.05,
   freeScrollFriction: .1 
 });
+
+
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
